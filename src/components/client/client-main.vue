2aa814b626d63eba88b0604c7ab8dc4058034c46
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2023-11-22 13:22:09 +0300

    Client - nav categories - added scroll navigation

--- src/components/client/client-main.vue
+++ src/components/client/client-main.vue
@@ -7,6 +7,12 @@ setBrowserTitleForClient()
 
 getModelAxios('companies', 1)
 getModelsAxios('categories')
+  .then(() => {
+    let divCategories = document.querySelector('.categories');
+    let scrollPaddingTop = divCategories.offsetHeight + 20 +'px'
+    console.log(scrollPaddingTop);
+    document.documentElement.style.setProperty('--scroll-padding-top', scrollPaddingTop);
+  })
 
 const btnBurgerMenu = ref(null)
 const burgerMenu = ref(null)
@@ -36,14 +42,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="info">
+  <section class="info">
     <div class="container">
       <div class="info__inner">
         <a class="info__item" href="#">О нас</a>
         <a class="info__item" href="#">Контакты</a>
       </div>
     </div>
-  </div>
+  </section>
 
   <header class="header">
     <div class="container">
@@ -77,8 +83,21 @@ onMounted(() => {
   <nav class="categories">
     <div class="container">
       <div class="categories__inner">
-        <a v-for="category in categories" class="categories__item" href="#">{{ category.title }}</a>
+
+        <a v-for="category in categories" class="categories__item" :href="'#' + category.title">
+          {{ category.title }}</a>
+
+        <div class="cart">
+          <h2>Корзина</h2>
+          <div>Пицца 1</div>
+          <div>Пицца 1</div>
+          <div>Пицца 1</div>
+          <div>Пицца 1</div>
+          <div>Итого</div>
+          <button>Заказать</button>
+        </div>
       </div>
+
     </div>
   </nav>
 
@@ -86,54 +105,37 @@ onMounted(() => {
     <i class="fa-solid fa-cart-shopping btn-cart-icon"></i>Корзина
   </button>
 
-  <div class="content">
+  <main class="content">
     <div class="container">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis odio repellendus rerum soluta
-      perferendis aut
-      deserunt veritatis at, labore praesentium sint exercitationem expedita saepe quisquam nobis nihil quo! Itaque
-      consectetur vero quisquam, id architecto mollitia dicta necessitatibus placeat nam molestiae at nemo tempora ea
-      officiis, dolorum commodi ad. Ut numquam voluptates suscipit voluptate repudiandae iusto, quia laudantium minus ea
-      cupiditate repellat velit aut, saepe sit, dolor id dignissimos modi neque consequatur distinctio. Velit sed
-      reiciendis, saepe nihil incidunt possimus nulla temporibus in adipisci tempore dignissimos ipsa, iure quo! Et quasi
-      velit maiores, ullam rerum corrupti odio accusantium, facere blanditiis accusamus atque ratione neque eius sequi
-      possimus perspiciatis saepe eos cupiditate autem sapiente quidem iste. Est doloribus aspernatur deleniti id
-      voluptatem
-      sed harum praesentium eligendi repudiandae nobis ipsa rerum ratione, optio officia, perferendis accusantium atque
-      eos
-      accusamus, reprehenderit dolores. Totam accusantium officiis distinctio quas odio excepturi, explicabo qui nesciunt,
-      reprehenderit, vitae necessitatibus delectus consequuntur recusandae ducimus nobis architecto velit eligendi ullam
-      sed
-      ratione magni doloremque repudiandae placeat quo? Quisquam temporibus, maxime ratione autem accusantium itaque,
-      corporis reprehenderit earum fugit tempora, vero dolorum culpa nihil expedita quos error quae a. Minima distinctio
-      atque voluptate iure itaque, culpa sit? Dignissimos officia modi repellendus excepturi? Voluptatum cupiditate quis
-      quisquam minus laboriosam ullam laudantium neque modi. Facere eveniet repellendus quos in ea ipsam veritatis
-      recusandae neque ipsa voluptatem pariatur, enim suscipit sint, accusamus blanditiis omnis aut quam fuga laborum
-      aspernatur repellat doloremque voluptates adipisci! Enim similique ullam id corrupti accusantium recusandae
-      asperiores
-      molestiae vel. Temporibus necessitatibus corrupti, illum esse at nam quis est odio asperiores optio deserunt eveniet
-      voluptates suscipit voluptate neque aspernatur perferendis nihil cupiditate vero aliquid ipsam mollitia tenetur.
-      Aspernatur non dicta incidunt nemo ratione culpa quia modi quasi velit. Delectus autem tenetur, dolorum ipsa nisi
-      blanditiis quis hic eum, aliquam vitae assumenda, eaque libero illo neque. Delectus, sed, voluptatem dolores
-      assumenda
-      ipsam alias facere eaque dolorum voluptate magnam quis doloribus, quaerat corporis nesciunt dicta eum? Ullam nam
-      ipsa
-      earum, rerum labore similique repudiandae quis repellendus vitae quibusdam laborum, est, nesciunt reiciendis maxime
-      veritatis. Quo maiores perspiciatis aut reiciendis quae eveniet, ad quas alias, culpa repellendus aspernatur sint
-      soluta id vitae eum? Consectetur architecto dolorum provident accusantium, fugit ab rem quisquam minima aperiam
-      corporis rerum soluta quod mollitia ea iste excepturi earum asperiores, suscipit corrupti sapiente consequuntur
-      reiciendis maxime aut! Officia reprehenderit ipsa voluptatibus quibusdam corporis fuga, itaque non officiis,
-      molestiae
-      dolorem illo dolores explicabo? Mollitia culpa, reiciendis, molestias repudiandae itaque ex ipsa voluptatibus velit
-      obcaecati illum quasi tempora sapiente voluptatum. Magni inventore quo sunt. Placeat perferendis a aliquid vitae
-      nobis
-      dolore minus! Repellat eum facilis vero velit voluptas? Quae magni sit, voluptatem fuga iure rem sed, quo earum
-      ratione deserunt, consequuntur est reiciendis laudantium asperiores atque obcaecati sint nihil animi quas saepe.
-      Porro
-      culpa commodi adipisci eos quam consectetur quis sequi exercitationem, necessitatibus quos voluptatem consequatur
-      quia
-      iusto officia nesciunt illo facere non labore ex obcaecati dicta? Alias quisquam dolorum corporis omnis corrupti ea
-      doloremque veniam? Maxime ipsa quasi temporibus numquam blanditiis.
+      <div class="content__inner">
+
+        <template v-for="category in categories">
+          <template v-if="category.products.length > 0">
+
+            <h2 :id="category.title" class="content__products__category-title">{{ category.title }}</h2>
+
+            <section class="content__products__category-section">
+              <div class="product-card" v-for="product in category.products">
+
+                <div>
+                  <img class="product-card__image" :src="product.image_url" alt="">
+                  <p class="product-card__title"> {{ product.title }}</p>
+                  <p class="product-card__description-short"> {{ product.description_short }}</p>
+                </div>
+
+                <div class="product-card__price-and-btn">
+                  <p class="product-card__price"> {{ Number(product.price_default) }} р.</p>
+                  <button type="button" class="product-card__btn-cart">В корзину</button>
+                </div>
+
+              </div>
+            </section>
+
+          </template>
+        </template>
+
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import router from "./router"
 let server_local = 'http://127.0.0.1:8000/api/v1'
 let server_hosting = 'https://u2315899.isp.regruhosting.ru/api/v1'
 
-export const serverApiUrl = server_local
+export const serverApiUrl = server_hosting
 
 createApp(App).use(router).mount('#app')
 
