32809d0a98ddd7c266bb70b056561b34c033946d
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-03-10 21:24:50 +0300

    Client - Added order panel for device

--- src/components/client/client-cart.vue
+++ src/components/client/client-cart.vue
@@ -1,13 +1,44 @@
 <script setup>
+import { ref } from 'vue';
 import {
   productsInCart, totalPrice,
   minusProductInCartForCartPanel,
   plusProductToCart, removeProductFromCart
 } from '/src/store/client-helper.js'
 
+const orderPanel = ref(null)
+
+function openOrderPanel() {
+  orderPanel.value.show()
+  document.body.classList.add('lock')
+
+  setTimeout(() => {
+    document.addEventListener('click', checkClickAndCloseOrderPanel)
+  }, 1000)
+}
+
+function closeOrderPanel() {
+  document.body.classList.remove('lock')
+  if (orderPanel.value) orderPanel.value.close()
+
+  document.removeEventListener('click', checkClickAndCloseOrderPanel)
+}
+
+function checkClickAndCloseOrderPanel(e) {
+  if (e.composedPath().includes(orderPanel.value)) return //если клик по панели, то ничего не делать
+  else closeOrderPanel()
+}
+
 </script>
 
+<style>
+.lock {
+  overflow: hidden;
+}
+</style>
+
 <template>
+
   <div class="cart-panel-device">
     <div class="cart-panel__header">Корзина</div>
 
@@ -22,7 +53,7 @@ import {
         <button class="cart-panel__product-btn-helpers" @click="minusProductInCartForCartPanel(product)">
           <i class="fa-solid fa-minus"></i></button>
         <div>{{ product.countInCart }}</div>
-        <button class="cart-panel__product-btn-helpers" @click="plusProductToCart(product)">
+        <button class="cart-panel__product-btn-helpers" @click.prevent="plusProductToCart(product)">
           <i class="fa-solid fa-plus"></i></button>
         <div>x {{ Number(product.price_default) }}р</div>
       </div>
@@ -35,7 +66,10 @@ import {
   </div>
   <div class="cart-panel__total-order-device">
     <div class="cart-panel__total">Итого: {{ totalPrice }}р.</div>
-    <button class="cart-panel__btn-order">Заказать</button>
+    <button class="cart-panel__btn-order" @click.prevent="openOrderPanel()">Заказать</button>
   </div>
-</template>
 
+  <dialog class="order-panel" ref="orderPanel">
+    Привет мир!
+  </dialog>
+</template>
