daf7310f2bc83631076436f1e35b7342f51e9736
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-03-17 13:45:48 +0300

    Client - Order panel - Added inputs

--- src/components/client/client-order-panel.vue
+++ src/components/client/client-order-panel.vue
@@ -18,14 +18,10 @@ onMounted(() => {
 //выход из панели заказа при клике вне панели заказа
 function checkClickAndCloseOrderPanel(e) {
   if (e.composedPath().includes(orderPanel.value)) return //если клик по панели, то ничего не делать
-  else {
-    document.body.classList.remove('order-panel-lock-scroll')
-    document.removeEventListener('click', checkClickAndCloseOrderPanel)
-    router.back()
-  }
+  else router.back()
 }
 
-//действия, если выход из панели заказа произошел иным способом (кнопка назад или клик по роуту)
+//действия при выходе из панели заказа (клик вне панели, кнопка назад или клик по ссылке)
 router.beforeEach((to, from, next) => {
   document.body.classList.remove('order-panel-lock-scroll')
   document.removeEventListener('click', checkClickAndCloseOrderPanel)
@@ -40,14 +36,16 @@ router.beforeEach((to, from, next) => {
       <h3 class="order-panel__title">Оформление заказа</h3>
       <div class="order-panel__form">
         <div>
-          <label>Адрес</label>
+          <label class="order-panel__label">Адрес</label>
           <input class="order-panel__adress" type="adress" placeholder="Введите адрес">
-          <label>Телефон</label>
+
+          <label class="order-panel__label">Телефон</label>
           <input class="order-panel__phone" type="phone" placeholder="Введите номер телефона">
         </div>
 
-        <button class="order-panel__btn-order">Оформить за {{ totalPrice }}р.</button>
+        <button class="order-panel__btn-order">ОФОРМИТЬ ЗА {{ totalPrice }}р.</button>
       </div>
+      <button class="order-panel__btn-close" @click.prevent="router.back()"><i class="fa-solid fa-xmark"></i></button>
     </div>
   </div>
 </template>
