b9fd999df08b59f2be2af8ebc2b381ca203963e5
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-04-01 18:00:26 +0300

    Client - Refactored css for popup and dialog forms

--- src/components/client/login-panel.vue
+++ src/components/client/login-panel.vue
@@ -1,9 +1,15 @@
-<template>
-      <div class="login-panel__form">
-        <div>
-          <input class="login-panel__phone" type="tel" placeholder="Введите номер телефона">
-        </div>
+<script setup>
 
-        <button class="login-panel__btn-order">Отправить код</button>
+</script>
+
+<template>
+  <div class="dialog">
+    <div class="dialog__form">
+      <div>
+        <input class="dialog__phone" type="tel" placeholder="Введите номер телефона">
       </div>
+
+      <button class="dialog__btn-submit">Отправить код</button>
+    </div>
+  </div>
 </template>
--- src/components/client/order-panel.vue
+++ src/components/client/order-panel.vue
@@ -4,15 +4,15 @@ import { totalPrice } from '/src/store/client-helper.js'
 </script>
 
 <template>
-  <div class="order-panel">
-    <h3 class="order-panel__title">Оформление заказа</h3>
-    <div class="order-panel__form">
+  <div class="dialog">
+    <h3 class="dialog__title">Оформление заказа</h3>
+    <div class="dialog__form">
       <div>
-        <input class="order-panel__adress" type="text" placeholder="Введите адрес">
-        <input class="order-panel__phone" type="tel" placeholder="Введите номер телефона">
+        <input class="dialog__adress" type="text" placeholder="Введите адрес">
+        <input class="dialog__phone" type="tel" placeholder="Введите номер телефона">
       </div>
 
-      <button class="order-panel__btn-order">ОФОРМИТЬ ЗА {{ totalPrice }}р.</button>
+      <button class="dialog__btn-submit">ОФОРМИТЬ ЗА {{ totalPrice }}р.</button>
     </div>
   </div>
 </template>
