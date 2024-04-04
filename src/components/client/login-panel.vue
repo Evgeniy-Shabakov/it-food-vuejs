d80a38424bcf780d1dbe64332e8325ed96198a55
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-04-04 12:46:43 +0300

    Client - Auth - test login panel

--- src/components/client/login-panel.vue
+++ src/components/client/login-panel.vue
@@ -1,15 +1,39 @@
 <script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { serverUrl } from '/src/main.js'
 
+const inputedPhone = ref('')
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+async function login() {
+  await axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
+    .then(res => {
+      console.log(res)
+    })
+  await axios
+    .post(`http://127.0.0.1:8000/send-verify-code`, { phone: inputedPhone.value })
+    .then(res => {
+      console.log(res);
+      
+    })
+    .catch(err => {
+      
+    })
+
+}
 </script>
 
 <template>
   <div class="dialog">
     <div class="dialog__form">
       <div>
-        <input class="dialog__phone" type="tel" placeholder="Введите номер телефона">
+        <input v-model="inputedPhone" class="dialog__phone" type="tel" placeholder="Введите номер телефона">
       </div>
 
-      <button class="dialog__btn-submit">Отправить код</button>
+      <button @click.prevent="login()" class="dialog__btn-submit">Отправить код</button>
     </div>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,22 @@
 import './css'
-import './vh-mobile-helper'
+import '/src/store/vh-mobile-helper'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from "./router"
 
-let server_local = 'http://127.0.0.1:8000/api/v1'
-let server_hosting = 'https://api.food-it.ru/api/v1'
+const server_local = 'http://127.0.0.1:8000'
+const server_hosting = 'https://api.food-it.ru'
 
-export const serverApiUrl = server_hosting
+const server_api_local = 'http://127.0.0.1:8000/api/v1'
+const server_api_hosting = 'https://api.food-it.ru/api/v1'
+
+export const serverUrl = server_hosting
+export const serverApiUrl = server_api_hosting
 
 createApp(App).use(router).mount('#app')
+
+
 
 
