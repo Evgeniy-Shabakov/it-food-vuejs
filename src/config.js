2adb8896ba43916847601addb7918b8f23c71ec2
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2024-04-05 20:31:49 +0300

    Revert "Axios - export config"
    This reverts commit 50f441cdcc361b7536da31e99bbe3d29462af74e.

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const server_local = 'http://localhost:8000'
-const server_api_local = server_local + '/api/v1'
-
-const server_hosting = 'https://api.food-it.ru'
-const server_api_hosting = server_hosting + '/api/v1'
-
-export const serverUrl = server_local
-export const serverApiUrl = server_api_local
