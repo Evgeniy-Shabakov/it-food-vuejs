bbfb96bad088e2d399a185e5a6ef44fb8118e04a
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-03-06 12:51:14 +0300

    Client - Orders history - refactored css

--- src/components/client/popup-pages/user/orders-history.vue
+++ src/components/client/popup-pages/user/orders-history.vue
@@ -46,13 +46,13 @@ else {
                         </p>
                         <p class="orders-history__order-status">({{ order.order_status }})</p>
 
-                        <div class="order-panel__products-section">
+                        <div class="user-panel__products-section">
 
                             <template v-for="product in order.products">
 
-                                <img class="order-panel__product-img" :src="product.image_url" alt="">
+                                <img class="user-panel__product-img" :src="product.image_url" alt="">
                                 <span>{{ product.title }}</span>
-                                <span class="order-panel__count-price"> {{ product.quantity }} </span>
+                                <span class="user-panel__count-price"> {{ product.quantity }} </span>
                                 <span> шт. </span>
 
                             </template>
